5dac980f6cfe45342216bf823e2d9d851cc70b6e
Léon Brocard <acme@astray.com>
2004-04-27 10:27:39 +0000

    Import of LBROCARD/DateTime-Stringify-4.10 from CPAN.
    gitpan-cpan-distribution: DateTime-Stringify
    gitpan-cpan-version:      4.10
    gitpan-cpan-path:         LBROCARD/DateTime-Stringify-4.10.tar.gz
    gitpan-cpan-author:       LBROCARD
    gitpan-cpan-maturity:     released

--- lib/DateTime/Stringify.pm
+++ lib/DateTime/Stringify.pm
@@ -2,14 +2,7 @@ package DateTime::Stringify;
 use strict;
 use DateTime;
 use vars qw($VERSION);
-$VERSION = '4.08';
-
-sub import {
-  eval q[
-package DateTime;
-use overload '""' => sub { return shift->datetime }
-  ]; 
-}
+$VERSION = '4.10';
 
 1;
 
@@ -17,12 +10,11 @@ __END__
 
 =head1 NAME
 
-DateTime::Stringify - DateTime objects
+DateTime::Stringify - DateTime objects (no longer needed)
 
 =head1 SYNOPSIS
 
   use DateTime;
-  use DateTime::Stringify;
 
   my $dt = DateTime->now;
   print "The time is now $dt...\n";
@@ -31,14 +23,13 @@ DateTime::Stringify - DateTime objects
 =head1 DESCRIPTION
 
 The DateTime suite of modules are a comprehensive way of representing
-and manipulating dates and times. However, interpolating a DateTime
-object in a string results in the not-so-useful
-"DateTime=HASH(0x800368)" or similar.
+and manipulating dates and times. This module used to be needed as
+interpolating a DateTime object in a string results in the
+not-so-useful "DateTime=HASH(0x800368)" or similar. However, DateTime
+has now incorporated the functionality of this module (as of DateTime
+0.21). Thus, this module is no longer needed.
 
-This module allows DateTime objects interpolated in strings to instead
-print out their $dt->datetime representation (this is also the
-$dt->iso8601 representation). This is probably more suitable for
-programmers (debugging!) than end-users.
+This module is just a placeholder now. Just use DateTime instead.
 
 =head1 AUTHOR
 
--- t/simple.t
+++ t/simple.t
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 use lib 'lib';
-use Test::More tests => 6;
+use Test::More tests => 3;
 use_ok("DateTime");
 
 my $dt = DateTime->new(year   => 1964,
@@ -12,20 +12,6 @@ my $dt = DateTime->new(year   => 1964,
                        second => 47,
                        nanosecond => 500000000,
                      );
-
-is($dt->datetime, "1964-10-16T16:12:47");
-like("$dt", qr/DateTime=HASH/);
-
-use_ok("DateTime::Stringify");
-
-$dt = DateTime->new(year   => 1964,
-		    month  => 10,
-                    day    => 16,
-                    hour   => 16,
-                    minute => 12,
-                    second => 47,
-		    nanosecond => 500000000,
-		   );
 
 is($dt->datetime, "1964-10-16T16:12:47");
 is("$dt", "1964-10-16T16:12:47");
